8efd8750e7590edcea685b6655c8fab942bb7bc8
rizki113070213 <rizki113070213@gmail.com>
2019-03-10 22:44:05 +0700

    Integrated technology model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  has_many :technologies
+
   def set_defaults
     # self.main_image ||= "https://via.placeholder.com/600x400"
     # self.thumb_image ||= "https://via.placeholder.com/350x200"
