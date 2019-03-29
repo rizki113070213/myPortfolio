ec25eb3e8634b0ccef0b9f0ab6133ecccf98ec0e
rizki113070213 <rizki113070213@gmail.com>
2019-03-29 17:50:33 +0700

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def login_helper style=""
     if !user_signed_in?
-      (link_to "Log In", new_user_session_path, class: style) +
+      (link_to "Log In", new_user_session_path, class: "#{style} #{active? new_user_session_path}") +
       " ".html_safe +
-      (link_to "Sign Up", new_user_registration_path, class: style)
+      (link_to "Sign Up", new_user_registration_path, class: "#{style} #{active? new_user_registration_path}")
     else
       link_to "Log Out", destroy_user_session_path, method: :delete, class: style
     end
@@ -18,5 +18,43 @@ module ApplicationHelper
 
   def copyright_generator
     TigaribuLabsViewTool::Renderer.copyright 'Muhammad Rizki', 'All rights reserved'
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: about_me_path,
+        title: "About Me"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: portfolios_path,
+        title: "Portfolio"
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ""
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href=#{item[:url]} class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 end
