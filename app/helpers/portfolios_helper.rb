e3e2aeed18cc4562293217b5b3aa1d4612959ecf
rizki113070213 <rizki113070213@gmail.com>
2019-04-18 20:11:20 +0700

    Updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,15 @@
 module PortfoliosHelper
+  def image_generator(height, width)
+    "https://via.placeholder.com/#{height}x#{width}"
+  end
+
+  def portfolio_img img, type
+    if img
+      img
+    elsif type == "thumb"
+      image_generator('350', '200')
+    else
+      image_generator('600', '400')
+    end
+  end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,21 +1,12 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-  after_initialize :set_defaults
-  validates_presence_of :title, :body, :main_image, :thumb_image
-
-  mount_uploader :main_image, PortfolioUploader
-  mount_uploader :thumb_image, PortfolioUploader
-
-  has_many :technologies, dependent: :destroy
+  has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  def set_defaults
-    # self.main_image ||= "https://via.placeholder.com/600x400"
-    # self.thumb_image ||= "https://via.placeholder.com/350x200"
-    self.main_image ||= Placeholder.image_generator(600, 400)
-    self.thumb_image ||= Placeholder.image_generator(350, 200)
-  end
+  validates_presence_of :title, :body
+
+  mount_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
 
   def self.by_position
     order("position ASC")
