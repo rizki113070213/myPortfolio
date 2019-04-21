01b951812acb510cf7515be02c96b4e6b73d81ca
rizki113070213 <rizki113070213@gmail.com>
2019-04-21 18:46:44 +0700

    Implemented custom scope for recent blogs and hide draft posts from non admins

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,4 +6,8 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body
   belongs_to :topic
   has_many :comments, dependent: :destroy
+
+  def self.recent
+    order("updated_at DESC")
+  end
 end
