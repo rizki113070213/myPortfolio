f4107f8355814b209a6c9ea052ce2bd7bfa5b1c8
rizki113070213 <rizki113070213@gmail.com>
2019-04-21 22:14:02 +0700

    Implemented the blog widget for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout "blog"
 
   def show
@@ -8,6 +9,16 @@ class TopicsController < ApplicationController
       @blogs = @topic.blogs.page(params[:page]).per(5)
     else
       @blogs = @topic.blogs.published.page(params[:page]).per(5)
+    end
+  end
+
+  private
+
+  def set_sidebar_topics
+    if logged_in?(:site_admin)
+      @sidebar_topics = Topic.with_blogs ""
+    else
+      @sidebar_topics = Topic.with_blogs "draft"
     end
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,12 @@
 class Topic < ApplicationRecord
   validates_presence_of :title
   has_many :blogs
+
+  def self.with_blogs status
+    if status == ""
+      includes(:blogs).where.not(blogs: {id: nil})
+    else
+      includes(:blogs).where.not(blogs: {id: nil, status: status})
+    end
+  end
 end
