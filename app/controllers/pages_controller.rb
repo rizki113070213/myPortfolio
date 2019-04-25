b5cceb410329afe5de3ee886dac3102cce5a1c17
rizki113070213 <rizki113070213@gmail.com>
2019-04-25 12:56:27 +0700

    Added view component for skills

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   end
 
   def about
+    @skills = Skill.utilized
   end
 
   def contact
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,7 @@
 class Skill < ApplicationRecord
   validates_presence_of :title, :percent_utilized
+
+  def self.utilized
+    all.order("percent_utilized desc")
+  end
 end
